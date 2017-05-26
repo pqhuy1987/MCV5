bb8094676e59fc82ae1ae73d4515681a9b0d5e51
pqhuy1987 <pqhuy1987@gmail.com>
2017-05-26 11:07:09 +0700

    Add file justified-nav.css in BundleConfig.cs

--- MCV5/App_Start/BundleConfig.cs
+++ MCV5/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace MCV5
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/justified-nav.css"));
         }
     }
 }
